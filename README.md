8d450d8a0d57744b802f37c04300e15866269c7d
Víctor Cardozo <vcardozof@gmail.com>
2021-02-18 13:03:37 +0100

    Changes project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# UdemyTest
+# PokeApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'udemy-test'`, () => {
+  it(`should have as title 'poke-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('udemy-test');
+    expect(app.title).toEqual('poke-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('udemy-test app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('poke-app app is running!');
   });
 });
